b989bc5cf8f50dc431a39681f8574d98e42558f1
Winnie <wimacoj@gmail.com>
2024-01-26 14:13:24 +0300

    Completed assignment

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,11 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ['some knowledge']  # Initializing with a list containing at least one element
 
     def teach(self):
-        pass
+        if not self.knowledge:
+            return None
+        return self.knowledge[0]
